0f04f2e9f55907207708a3137cf21caf85784c4f
codewhight <161123216+codewhight@users.noreply.github.com>
2024-03-22 19:47:57 +0800

    Update hw7_matrixMul.js

--- JavaScript/HW1/hw7_matrixMul.js
+++ JavaScript/HW1/hw7_matrixMul.js
@@ -1,1 +1,23 @@
+//hw8
+function matrixMul(A,B)
+{
+    let sum = [];
+    for(let i = 0;i < 2;i++)
+    {
+        sum[i] = [];
+        for(let j = 0;j < 2;j++)
+        {
+            sum[i][j] = 0;
+            for(let k = 0;k < 2;k++)
+            {
+                sum[i][j] += A[i][k] * B[k][j];
+            }
+        }
+
+    }
+    return sum;
+}
+
+var a1 =[[1,2],[3,4]], a2 = [[5,6],[7,8]];
+console.log(matrixMul(a1,a2));
 
