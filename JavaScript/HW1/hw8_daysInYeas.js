0bdc15189d90d89de6401b4caf6c2d9462210911
codewhight <161123216+codewhight@users.noreply.github.com>
2024-03-22 18:55:03 +0800

    Update hw8_daysInYeas.js

--- JavaScript/HW1/hw8_daysInYeas.js
+++ JavaScript/HW1/hw8_daysInYeas.js
@@ -1,1 +1,19 @@
-
+function DayInYear(n)
+{
+    if(n % 4 == 0)
+    {
+        if(n % 100 == 0 && n % 400 != 0)
+        {
+            return 365;
+        }
+        return 366;
+    }
+    else
+    {
+        return 365;
+    }       
+}
+console.log('DayInYear(1991)',DayInYear(1991));
+console.log('DayInYear(2000)',DayInYear(2000));
+console.log('DayInYear(2004)',DayInYear(2004));
+console.log('DayInYear(1900)',DayInYear(1900));
